89b9e0969cd1582c91db96ac6ab7089413e21d83
Pawel Sioda <pawel.sioda@gmail.com>
2023-09-05 22:39:34 +0200

    add price formating

--- src/ui/atoms/ProductsListItemDescription.tsx
+++ src/ui/atoms/ProductsListItemDescription.tsx
@@ -1,4 +1,5 @@
 import { type Product } from "@/types/product";
+import { formatMoney } from "@/utils";
 
 type ProductsListItemDescriptionProps = {
 	product: Product;
@@ -8,7 +9,7 @@ export const ProductsListItemDescription = ({ product }: ProductsListItemDescrip
 	return (
 		<>
 			<h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-			<p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+			<p className="mt-1 text-lg font-medium text-gray-900">{formatMoney(product.price / 100)}</p>
 		</>
 	);
 };
