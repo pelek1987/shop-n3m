596e329a6a33bfce63b411ca995a901f67b17596
Pawel Sioda <pawel.sioda@gmail.com>
2023-09-18 20:42:09 +0200

    change ProductsListItem to link

--- src/ui/molecules/ProductsListItem.tsx
+++ src/ui/molecules/ProductsListItem.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
 import { ProductsListItemCoverImage } from "../atoms/ProductsListItemCoverImage";
 import { ProductsListItemDescription } from "../atoms/ProductsListItemDescription";
 import { type Product } from "@/types/product";
+import { AppRoutes } from "@/types/app-routes";
 
 type ProductsListItemProps = {
 	product: Product;
@@ -9,8 +11,10 @@ type ProductsListItemProps = {
 export const ProductsListItem = ({ product }: ProductsListItemProps) => {
 	return (
 		<li className="group">
-			<ProductsListItemCoverImage coverImage={product.coverImage} />
-			<ProductsListItemDescription product={product} />
+			<Link href={`${AppRoutes.PRODUCTS}/${product.id}`}>
+				<ProductsListItemCoverImage coverImage={product.coverImage} />
+				<ProductsListItemDescription product={product} />
+			</Link>
 		</li>
 	);
 };
